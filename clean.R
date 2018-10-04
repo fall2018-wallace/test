31a4bf432d2e0d13926a508b696533e06287ce21
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 12:53:14 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -3,6 +3,7 @@ clean_data <- raw_data
 #re-use the code from before
 readStates<-function(states)
 {
-    states<-states[-1,]
-    num.row<-nrow(states)
+    states<- states[-1,]
+    num.row<- nrow(states)
+    states<- states[-num.row,]
 }
